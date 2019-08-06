83eb8afc42655db314c1635f65b79bd269d4a416
Rosemari Burton <rosemariburtonCB5@MacBook-Air-2.local>
2019-08-06 18:53:14 -0400

    added more detailed  pseudocoding

--- javascript/app.js
+++ javascript/app.js
@@ -1,16 +1,33 @@
 // #### EVENT HANDLER SECTIN
-// #### LOAD
-// #### ERROR
-// #### PLAYER CLICK
+// ---- Load
+// ---- Error
+// ---- Player Click
 
+// #### SET GLOBAL VARIABLES SECTION
 
-// #### FUNCTIONS
-// #### SET LEVEL
-// #### GET SIMON ARRAY
-// #### STEP THRU SIMON ARRAY
-// #### CLICK
-// #### IS CORRECT
-// #### IS END
-
+// #### FUNCTIONS SECTION
+// ---- Signup (Create and store new user info)
+// ---- Login (Retrieve user info)
+// ---- Set Difficulty Level
+// ---- Get Simon Array
+// ---- Step Thru Simon Array
+// ---- 4 Second Wait For User Input
+// ---- Is Correct
+// ---- Update Score
+// ---- Update Player Array
+// ---- Is End
+// ---- Losing Wrap-up
+// ---- Winning Wrap-up
+// ---- Is End
 
 // #### DATA
+// ---- Simon Sequence
+// ---- Player Sequence
+// ---- Top Five (Stretch Goal)
+// ---- 
+// ---- 
+// ---- 
+// ---- 
+// ---- 
+// ---- 
+// ---- 
