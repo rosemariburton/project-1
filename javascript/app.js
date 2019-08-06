c407ab46da8e724a7b5d3f8d3e9f8d6aae467fc7
Rosemari Burton <rosemariburtonCB5@MacBook-Air-2.local>
2019-08-06 18:34:55 -0400

    more pseudocoding

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,16 @@
 // #### EVENT HANDLER SECTIN
+// #### LOAD
+// #### ERROR
+// #### PLAYER CLICK
+
 
 // #### FUNCTIONS
+// #### SET LEVEL
+// #### GET SIMON ARRAY
+// #### STEP THRU SIMON ARRAY
+// #### CLICK
+// #### IS CORRECT
+// #### IS END
+
 
 // #### DATA
